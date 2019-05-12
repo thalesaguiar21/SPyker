6d1a695a5983f635482d9acfab38152e59e7bbd8
Thales Aguiar <thalesaguiar21@gmail.com>
2019-05-12 18:29:13 -0300

    Fixed mlp generator Added enumeration to access the rows of the grid search

--- spyker/identification.py
+++ spyker/identification.py
@@ -1,5 +1,9 @@
 import sklearn.neural_network as skneural
 import sklearn.model_selection as skselection
+import numpy as np
+import pdb
+from itertools import product
+from enum import Enum, auto
 
 
 class _Identifier:
@@ -14,9 +18,14 @@ class _Identifier:
 def create_trained_identifier(X, y):
     best_accuracy = 0
     best_mlp = None
+    curr_mlp = 0
     for mlp in _mlp_generator():
-        if train_and_test(X, y, mlp) > best_accuracy:
+        curr_mlp += 1
+        msg = 'Training and testing MLP for row {} from grid...'
+        print(msg.format(curr_mlp), end='\r')
+        if train_and_test(mlp, X, y) > best_accuracy:
             best_mlp = mlp
+    print('Best MLP achieved', best_accuracy, 'accuracy!')
     return _Identifier(best_mlp)
 
 
@@ -35,4 +44,35 @@ def train_and_test(mlp, X, y):
 
 def _mlp_generator():
     """ Do a grid search over MLPClassifier """
-    pass
+    hidden_layer_step = [(_MIN_HL_SIZE + t,) for t in range(0, 201, 50)]
+    activations = ['identity', 'logistic', 'tanh', 'relu']
+    solvers = ['lbfgs', 'sgd', 'adam']
+    learnrate = [0.0001, 0.001, 0.01]
+    max_iters = [x for x in range(100, 501, 100)]
+    tols = [0.0001, 0.001, 0.01]
+    n_iter_no_changes = [x for x in range(5, 31, 5)]
+    grid = product(hidden_layer_step, activations, solvers, learnrate,
+                   max_iters, tols, n_iter_no_changes)
+    for row in grid:
+        mlp = skneural.MLPClassifier(
+                hidden_layer_sizes=row[GRIDIndexes.HLSIZE],
+                activation=row[GRIDIndexes.ACTIVATION],
+                solver=row[GRIDIndexes.SOLVER],
+                alpha=row[GRIDIndexes.ALPHA],
+                max_iter=row[GRIDIndexes.MAXITER],
+                tol=row[GRIDIndexes.TOL],
+                n_iter_no_change=row[GRIDIndexes.NITER])
+        yield mlp
+
+
+_MIN_HL_SIZE = 100
+
+
+class GRIDIndexes:
+    HLSIZE= 0
+    ACTIVATION = 1
+    SOLVER = 2
+    ALPHA = 3
+    MAXITER = 4
+    TOL = 5
+    NITER = 6
--- spyker/test.py
+++ spyker/test.py
@@ -3,5 +3,5 @@ import identification as spid
 
 
 X, y = skdata.make_blobs(
-    n_samples=100, centers=5, n_features=30, random_state=0)
-identifier = spid.train(X, y)
+    n_samples=50, centers=5, n_features=30, random_state=0)
+identifier = spid.create_trained_identifier(X, y)
