5c6db9123a5be90aa0df8f9580d41e3ee5b3add4
Thales Aguiar <thalesaguiar21@gmail.com>
2019-05-12 16:39:56 -0300

    Moved mlp selection responsibility to create_trained_identifier method

--- spyker/identification.py
+++ spyker/identification.py
@@ -11,25 +11,26 @@ class _Identifier:
         return self.detector.predict(Xi)
 
 
-def create_identifier():
-    pass
-
-
-def train(X, y):
-    loo = skselection.LeaveOneOut()
-    best_acc, best_mlp = 0.0, None
+def create_trained_identifier(X, y):
+    best_accuracy = 0
+    best_mlp = None
     for mlp in _mlp_generator():
-        hits = 0
-        for train_index, test_index in loo.split(X):
-            X_train, y_train = X[train_index], y[train_index]
-            X_test, y_test = X[test_index], y[test_index]
-            mlp.fit(X_train, y_train)
-            if mlp.predict(X_test) == y_test:
-                hits += 1
-        accuracy = hits/X.shape[0] * 100
-        if best_acc <= accuracy:
+        if train_and_test(X, y, mlp) > best_accuracy:
             best_mlp = mlp
     return _Identifier(best_mlp)
+
+
+def train_and_test(mlp, X, y):
+    loo = skselection.LeaveOneOut()
+    hits = 0
+    for train_index, test_index in loo.split(X):
+        X_train, y_train = X[train_index], y[train_index]
+        X_test, y_test = X[test_index], y[test_index]
+        mlp.fit(X_train, y_train)
+        if mlp.predict(X_test) == y_test:
+            hits += 1
+    accuracy = hits/X.shape[0] * 100
+    return accuracy
 
 
 def _mlp_generator():
